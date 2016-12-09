65a51dbecee8bd70a4164e198762a9553ccfeecc
Kaysser Emad Kayyali <kaykayyali@gmail.com>
2016-12-08 23:06:15 -0500

    Reduce joystick events.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var Joystick = require('./lib/joystick');
 var Xbox_Interpreter = require('./lib/xbox_interpreter');
 // Set a deadzone of +/-3500 (out of +/-32k) and a sensitivty of 350 to reduce signal noise in joystick axis
-var controller = new Joystick(0, 3500, 350);
+var controller = new Joystick(0, 3500, 1000);
 var Interpreter = new Xbox_Interpreter();
 function init() {
 	console.log("Initializing...");
