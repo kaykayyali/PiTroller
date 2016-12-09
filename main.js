3fb55ae79608e7ff7eab5dc7c37085deb444fc6f
Kaysser Emad Kayyali <kaykayyali@gmail.com>
2016-12-08 21:36:29 -0500

    Wait a second

--- main.js
+++ main.js
@@ -3,13 +3,17 @@ var Xbox_Interpreter = require('./lib/xbox_interpreter');
 // Set a deadzone of +/-3500 (out of +/-32k) and a sensitivty of 350 to reduce signal noise in joystick axis
 var controller = new Joystick(0, 3500, 350);
 var Interpreter = new Xbox_Interpreter();
-
-controller.on('button', function(event_data) {
+function init() {
+	console.log("Initializing...");
+	controller.on('button', function(event_data) {
 	console.log("button event");
 	Interpreter.handle_button_event(event_data);
 
-});
-controller.on('axis', function(event_data) {
-	console.log("axis event");
-	Interpreter.handle_axis_event(event_data);
-});
+	});
+	controller.on('axis', function(event_data) {
+		console.log("axis event");
+		Interpreter.handle_axis_event(event_data);
+	});
+};
+
+setTimout(init, 3000);
