808eec76e060a3a2609ceb1f08edac11684cf772
Kaysser Emad Kayyali <kaykayyali@gmail.com>
2016-12-08 21:28:40 -0500

    First stab at xbox control.

--- example.js
+++ example.js
@@ -1,5 +1,13 @@
 var Joystick = require('./lib/joystick');
+var Xbox_Interpreter = require('./lib/xbox_interpreter');
 // Set a deadzone of +/-3500 (out of +/-32k) and a sensitivty of 350 to reduce signal noise in joystick axis
 var controller = new Joystick(0, 3500, 350);
-controller.on('button', console.log);
-controller.on('axis', console.log);
+var Interpreter = new Xbox_Interpreter();
+
+controller.on('button', function(event_data) {
+	Interpreter.handle_button_event(event_data);
+
+});
+controller.on('axis', function(event_data) {
+	Interpreter.handle_axis_event(event_data);
+});
